e98b05bdf1406eac319f047b5509f6d56733666e
walker <bin5211bin@gmail.com>
2018-05-04 16:00:24 +0800

    [创建了根控制器]

--- IOSCommonTools/AppDelegate.m
+++ IOSCommonTools/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [self configTabbarVC];
     return YES;
 }
 
@@ -94,5 +95,24 @@
         abort();
     }
 }
+
+#pragma mark - private
+- (void)configTabbarVC {
+    UITabBarController *tabbarVc = [[UITabBarController alloc] init];
+    UIViewController *firstVC = [[UIViewController alloc] init];
+    firstVC.view.backgroundColor = [UIColor orangeColor];
+    firstVC.title = @"第一个";
+    
+    UIViewController *secondVC = [[UIViewController alloc] init];
+    secondVC.view.backgroundColor = [UIColor grayColor];
+    secondVC.title = @"第2个";
+    secondVC.view.bounds = [UIScreen mainScreen].bounds;
+    
+    tabbarVc.viewControllers = [NSArray arrayWithObjects:firstVC, secondVC,nil];
+    self.window = [[UIWindow alloc] init];
+    self.window.rootViewController = tabbarVc;
+    [self.window makeKeyAndVisible];
+}
+
 
 @end
